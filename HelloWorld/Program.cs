fd500f9371a889ebd6c4621ad947ff7b5e3cb08b
Niklas Engvall <niklas_engvall@hotmail.com>
2022-09-17 17:08:13 +0200

    Renamed a project

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,7 @@
         {
             Console.WriteLine("Hello World, from Net6 with namespace etc!");
             Console.WriteLine("Hello Niklas!");
+            Console.Read();
         }
     }
 }
